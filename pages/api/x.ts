286d95af943617301328b623de7f5468af3b5987
xg4 <xingor4@gmail.com>
2022-10-22 15:05:02 +0800

    feat: compact urls

--- pages/api/x.ts
+++ pages/api/x.ts
@@ -1,4 +1,5 @@
 import retry from 'async-retry'
+import { compact } from 'lodash'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { generateConfig, getRemoteUrls } from '../../services'
 
@@ -11,12 +12,11 @@ export default async function handler(
       retries: 1,
     })
 
-    const configText = await retry(
-      () => generateConfig(Object.assign({ url: urls.join('|') }, req.query)),
-      {
-        retries: 1,
-      }
-    )
+    req.query.url = compact([...urls, req.query.url]).join('|')
+
+    const configText = await retry(() => generateConfig(req.query), {
+      retries: 1,
+    })
 
     res.setHeader('Content-Type', 'text/plain;charset=utf-8')
     res.status(200).send(configText)
