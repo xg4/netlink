9c500a3b19616af785bfe6339fc7c60a3979652b
xg4 <xingor4@gmail.com>
2023-05-18 05:33:35 +0000

    revert: "feat: add filter list"
    This reverts commit b79ae9dd514c6c5d79773e442b17e742ce83771c.

--- app/api/free/route.ts
+++ app/api/free/route.ts
@@ -1,20 +1,14 @@
 import getRemoteUrls from '@/services/urls'
 import { b64Encode } from '@/utils/base64'
-import { difference } from 'lodash'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
   const remoteUrls = await getRemoteUrls()
 
-  console.log(remoteUrls)
-
-  return new NextResponse(
-    b64Encode(difference(remoteUrls, process.env.FILTER_LIST ? process.env.FILTER_LIST.split('|') : []).join('\n')),
-    {
-      status: 200,
-      headers: {
-        'Content-Type': 'text/plain;charset=utf-8',
-      },
+  return new NextResponse(b64Encode(remoteUrls.join('\n')), {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/plain;charset=utf-8',
     },
-  )
+  })
 }
