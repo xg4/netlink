b79ae9dd514c6c5d79773e442b17e742ce83771c
xg4 <xingor4@gmail.com>
2023-05-18 04:57:25 +0000

    feat: add filter list

--- app/api/free/route.ts
+++ app/api/free/route.ts
@@ -1,14 +1,20 @@
 import getRemoteUrls from '@/services/urls'
 import { b64Encode } from '@/utils/base64'
+import { difference } from 'lodash'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
   const remoteUrls = await getRemoteUrls()
 
-  return new NextResponse(b64Encode(remoteUrls.join('\n')), {
-    status: 200,
-    headers: {
-      'Content-Type': 'text/plain;charset=utf-8',
+  console.log(remoteUrls)
+
+  return new NextResponse(
+    b64Encode(difference(remoteUrls, process.env.FILTER_LIST ? process.env.FILTER_LIST.split('|') : []).join('\n')),
+    {
+      status: 200,
+      headers: {
+        'Content-Type': 'text/plain;charset=utf-8',
+      },
     },
-  })
+  )
 }
